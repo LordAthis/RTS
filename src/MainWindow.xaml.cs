185be8719a91d5a430698ab1fbebe31336258314
LordAthis <lordathis@gmail.com>
2026-03-28 20:18:39 +0100

    Create MainWindow.xaml.cs

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+using System.Windows;
+using System;
+
+namespace RTS
+{
+    public partial class MainWindow : Window
+    {
+        private bool isDark = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Ablak mozgatása (Mivel nincs címsor)
+        protected override void OnMouseLeftButtonDown(System.Windows.Input.MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnThemeToggle_Click(object sender, RoutedEventArgs e)
+        {
+            // Egyszerű téma váltó logika
+            ResourceDictionary newTheme = new ResourceDictionary();
+            if (isDark)
+                newTheme.Source = new Uri("Themes/LightTheme.xaml", UriKind.Relative);
+            else
+                newTheme.Source = new Uri("Themes/DarkTheme.xaml", UriKind.Relative);
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(newTheme);
+            isDark = !isDark;
+        }
+    }
+}
