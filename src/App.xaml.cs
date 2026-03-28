f51a230bcd6315771800bf54b9842393f4a92031
LordAthis <lordathis@gmail.com>
2026-03-28 20:27:51 +0100

    Create App.xaml.cs

---
+++ src/App.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace RTS
+{
+    public partial class App : Application
+    {
+        // Ide kerülhet később az extra indítási logika (pl. naplózás indítása)
+    }
+}
